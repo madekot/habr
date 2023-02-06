6c228b140f64325e0e32c420c85473d0f2c4aded
Mikhail Golubtsov <mgolubcov@gmail.com>
2023-02-06 13:07:44 +0300

    роуты определил на уровне конфига, добавляет первый ui компонент ссылку и добавляет для него несколько тем

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,16 @@
 import "./styles/index.scss";
-import {Link, Route, Routes} from "react-router-dom";
-import {Suspense} from "react";
 import {classNames} from "shared/lib/classNames/classNames";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
 import {useTheme} from "app/providers/ThemeProvider";
+import {AppRouter} from "./providers/router";
+import {Navbar} from "widgets/Navbar";
 
 export const App = () => {
   const {toggleTheme, theme} = useTheme();
   return (
     <div className={classNames('app', {}, [theme])}>
+      <Navbar/>
+      <AppRouter/>
       <button onClick={toggleTheme}>setTheme</button>
-      <Link to={"/"}>Главная</Link>
-      <Link to={"/about"}>О сайте</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={'/about'} element={<AboutPage />} />
-          <Route path={'/'} element={<MainPage />} />
-        </Routes>
-      </Suspense>
     </div>
   );
 };
