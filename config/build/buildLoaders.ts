b42c4960d683bc42a3fb3d3f58cc3164e48803be
Mikhail Golubtsov <mgolubcov@gmail.com>
2023-02-07 20:12:26 +0300

    добавляет babel и Extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -13,6 +13,26 @@ export const buildLoaders = ({isDev}: BuildOptions): webpack.RuleSetRule[] => {
     ],
   }
 
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ['@babel/preset-env'],
+        plugins: [
+          [
+            "i18next-extract",
+            {
+              locales: ['ru', 'en'],
+              keyAsDefaultValue: true,
+            }
+          ]
+        ],
+      }
+    }
+  }
+
   const svgLoader = {
     test: /\.svg$/,
     use: ['@svgr/webpack'],
@@ -46,6 +66,7 @@ export const buildLoaders = ({isDev}: BuildOptions): webpack.RuleSetRule[] => {
   return [
     svgLoader,
     fileLoader,
+    babelLoader,
     typescriptLoader,
     cssLoader,
   ]
