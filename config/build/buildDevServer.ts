6c9922734d5b536afcd8fd21dc0238e4a497b591
Mikhail Golubtsov <mgolubcov@gmail.com>
2023-02-13 17:17:49 +0300

    установит плагин вебпака для анализа бандла BundleAnalyzer, это поможет следить за размерами бандла и своевременно декомпозировать

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -4,5 +4,5 @@ import { BuildOptions } from './types/config';
 export const buildDevServer = (options: BuildOptions): DevServerConfiguration => ({
     port: options.port,
     historyApiFallback: true,
-    // open: true
+    open: true,
 });
