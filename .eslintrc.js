0ce29723151bd049295a7eaac74740187d735af1
Mikhail Golubtsov <mgolubcov@gmail.com>
2023-02-14 12:38:30 +0300

    добавляет тесты для sidebar

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,8 +10,6 @@ module.exports = {
         'plugin:i18next/recommended',
     ],
     parser: '@typescript-eslint/parser',
-    overrides: [
-    ],
     parserOptions: {
         ecmaFeatures: {
             jsx: true,
@@ -43,10 +41,24 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
-        'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'i18next/no-literal-string': [
+            'error',
+            {
+                markupOnly: true,
+                ignoreAttribute: ['data-testid', 'to'],
+            },
+        ],
         'max-len': ['error', { ignoreComments: true, code: 100 }],
     },
     globals: {
         __IS_DEV__: true,
     },
+    overrides: [
+        {
+            files: ['**/src/**/*.test.{ts,tsx}'],
+            rules: {
+                'i18next/no-literal-string': 'off',
+            },
+        },
+    ],
 };
--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,13 +1,13 @@
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
 import { render, screen } from '@testing-library/react';
 
-describe('classNames', () => {
-    test('with only first param', () => {
+describe('Button', () => {
+    test('Test render', () => {
         render(<Button>TEST</Button>);
         expect(screen.getByText('TEST')).toBeInTheDocument();
     });
 
-    test('with only first param', () => {
+    test('Test clear theme', () => {
         render(<Button theme={ThemeButton.CLEAR}>TEST</Button>);
         expect(screen.getByText('TEST')).toHaveClass('clear');
         screen.debug();
