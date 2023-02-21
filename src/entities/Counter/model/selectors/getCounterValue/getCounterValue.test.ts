8207cb9cea5517ab21fcf2eeb0308dd563af9676
Mikhail Golubtsov <mgolubcov@gmail.com>
2023-02-21 13:45:02 +0300

    добавит redux store, добавит первый entity сущность

---
+++ src/entities/Counter/model/selectors/getCounterValue/getCounterValue.test.ts
@@ -0,0 +1,12 @@
+import { getCounterValue } from 'entities/Counter/model/selectors/getCounterValue/getCounterValue';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { DeepPartial } from '@reduxjs/toolkit';
+
+describe('getCounterValue.test', () => {
+    test('', () => {
+        const state: DeepPartial<StateSchema> = {
+            counter: { value: 10 },
+        };
+        expect(getCounterValue(state as StateSchema)).toEqual(10);
+    });
+});
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { BugButtons } from 'app/providers/ErrorBoudary';
+import { Counter } from 'entities/Counter';
 
 const MainPage = () => {
     const { t } = useTranslation();
 
     return (
         <div>
-            {/* <BugButtons /> */}
             {t('Главная страница')}
-            {/* {t('Новый перевод')} */}
+            <Counter />
         </div>
     );
 };
