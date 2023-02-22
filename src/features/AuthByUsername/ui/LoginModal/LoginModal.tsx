0002aa656a0769aea8e4938305e35f4fa3d57163
Mikhail Golubtsov <mgolubcov@gmail.com>
2023-02-22 14:22:30 +0300

    тест pre-commit

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,7 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Modal } from 'shared/ui/Modal/Modal';
 import { LoginForm } from '../LoginForm/LoginForm';
-import test from './LoginModal.module.scss';
 
 interface LoginModalProps {
   className?: string;
