3cd527e9c6fb3a2e0b0f28b5ad76a7d068ade502
Mikhail Golubtsov <mgolubcov@gmail.com>
2023-02-28 13:53:08 +0300

    save

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -46,7 +46,6 @@ export const Modal = (props: ModalProps) => {
         }
     }, [onClose]);
 
-    // Новые ссылки!!!
     const onKeyDown = useCallback((e: KeyboardEvent) => {
         if (e.key === 'Escape') {
             closeHandler();
